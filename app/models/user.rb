3edebc16b50190242c5c0720f0f74f5dffbbdd2f
EC2 Default User <ec2-user@ip-172-31-6-65.ap-northeast-1.compute.internal>
2023-05-02 06:46:10 +0000

    regular updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,16 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  # xxxはアソシエーションが繋がっているテーブル名、class_nameは実際のモデルの名前、foreign_keyは外部キーとして何を持つかを表しています。
+  #Relationshipテーブルを参照し、keyでカラムを選択
+  has_many :relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  #「has_many :テーブル名, through: :中間テーブル名」 の形を使って、テーブル同士が中間テーブルを通じてつながっていることを表現します。(followerテーブルとfollowedテーブルのつながりを表す）
+  # 例えば、yyyにfollowedを入れてしまうと、followedテーブルから中間テーブルを通ってfollowerテーブルにアクセスすることができなくなってしまいます。
+  # これを防ぐためにyyyには架空のテーブル名を、zzzは実際にデータを取得しにいくテーブル名を書きます。
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :relationships, source: :follower
+  
 
   #validates :title, length: { minimum: 2, maximum: 20 }, uniqueness: true
   #validates :title, length: { maximum: 20 }, uniqueness: true
@@ -27,4 +37,18 @@ class User < ApplicationRecord
     #end
     #profile_image
   end
+  
+  # フォローしたときの処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+# フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+# フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,12 @@ Rails.application.routes.draw do
    #なので、単数系にして、いいねの/:idがurlに含まれなくなってもよい
    resource :favorites, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
+     # ネストさせる
+   resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+   end
   end
   
   resources :users, only: [:index,:show,:edit,:update]
